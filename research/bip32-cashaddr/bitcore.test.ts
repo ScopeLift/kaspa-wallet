0135de41ec398b04e723e631a68f11d763e9b404
wildmolasses <changes@gmail.com>
2020-06-30 17:32:26 -0500

    small change

--- research/bip32-cashaddr/bitcore.test.ts
+++ research/bip32-cashaddr/bitcore.test.ts
@@ -1,23 +1,21 @@
 const bitcore = require('bitcore-lib-cash')
 
-// let networkAlias = "kaspatest";
-
 let networkPrefixes = ["kaspa", "kaspadev", "kaspareg", "kaspatest", "kaspasim"]
 
 networkPrefixes.map(str => {
   bitcore.Networks.add({
     name: str,
     prefix: str,
-    pubkeyhash: 28, //publickey hash prefix
+    pubkeyhash: 0x00, //publickey hash prefix
     privatekey: 0x80, // privatekey prefix -- must be 128 or toWIF() result will not match with kaspa 
-    scripthash: 40,
-    xpubkey: 0x043587cf, // extended public key magic
-    xprivkey: 0x04358394, // extended private key magic
-    // networkMagic: 0xdab5bffa, // network magic number
+    scripthash: 0x05,
+    xpubkey: 0x0488b21e, // extended public key magic
+    xprivkey: 0x0488ade4, // extended private key magic
+    networkMagic: 0xdab5bffa, // network magic number
   })
 })
 
-// bitcore.Networks.defaultNetwork = "kaspa"
+// bitcore.Networks.defaultNetwork = bitcore.Networks.kaspa
 
 let keyTest = {
   pubKeyHex:
