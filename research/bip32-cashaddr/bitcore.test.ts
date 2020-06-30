5024662a05ccebd2554e27b9e3daab6656510d5b
wildmolasses <changes@gmail.com>
2020-06-29 20:39:41 -0500

    check in

--- research/bip32-cashaddr/bitcore.test.ts
+++ research/bip32-cashaddr/bitcore.test.ts
@@ -1,43 +1,51 @@
-const bitcore = require("bitcore-lib-cash");
-// const bech = require("bitcore-lib/lib/encoding/bech32");
+const bitcore = require('bitcore-lib-cash')
 
-let networkAlias = "kaspatest";
+// let networkAlias = "kaspatest";
 
-let kaspaNetwork = bitcore.Networks.add({
-  name: "Kaspa test",
-  alias: networkAlias,
-  prefix: networkAlias,
-  pubkeyhash: 0x6, //publickey hash prefix
-  privatekey: 0xef, // privatekey prefix
-  scripthash: 0xc4,
-  xpubkey: 0x043587cf, // extended public key magic
-  xprivkey: 0x04358394, // extended private key magic
-  networkMagic: 0xdab5bffa, // network magic number
-  port: 18444, // network port
-  dnsSeeds: [],
+let networkPrefixes = ["kaspa", "kaspadev", "kaspareg", "kaspatest", "kaspasim"]
+
+networkPrefixes.map(str => {
+  bitcore.Networks.add({
+    name: str,
+    prefix: str,
+    pubkeyhash: 28, //publickey hash prefix
+    privatekey: 0x80, // privatekey prefix -- must be 128 or toWIF() result will not match with kaspa 
+    scripthash: 40,
+    xpubkey: 0x043587cf, // extended public key magic
+    xprivkey: 0x04358394, // extended private key magic
+    // networkMagic: 0xdab5bffa, // network magic number
+  })
+})
+
+// bitcore.Networks.defaultNetwork = "kaspa"
+
+let keyTest = {
+  pubKeyHex:
+    "02f707d9ce3bd3998744cb6bd3e3da4b68a5babef5b324948681ac9250723c8fc9",
+  pubKeyCashAddr: "kaspadev:qp02uemuxw2tva3fp9q5qunxxgk907r70uyxje5eg0",
+  privKeyHex:
+    "0a2abb86b8cc3c91ffa8efe01ec16b1efca24dd7e79538d3248f8b275e14f2f0",
+  wif: "KwZUSy3xbs5eSDCgDAoTSR5KSypjU2Uy4F1QJmmKVmk2QiEoYtP1"
+};
+
+test("kaspa-provided keypair should play nicely with bitcoin-lib-cash", () => {
+    let myAddress = new bitcore.Address(keyTest.pubKeyCashAddr);
+    let privateKey = new bitcore.PrivateKey(keyTest.privKeyHex);
+    expect(bitcore.Address.isValid(keyTest.pubKeyCashAddr)).toBe(true);
+    expect(myAddress.toString()).toEqual(keyTest.pubKeyCashAddr);
+    expect(bitcore.PrivateKey.isValid(keyTest.privKeyHex)).toBe(true);
+    expect(privateKey.toAddress("kaspadev").toString()).toEqual(keyTest.pubKeyCashAddr);
+    expect(privateKey.toWIF()).toEqual(keyTest.wif);
 });
 
-let kaspaKeyPairs = [
-  {
-    privKey: "tb1qw508d6qejxtdg4y5r3zarvary0c5xw7kxpjzsxawgt",
-    pubKeys: {
-      main: "kaspa:pr6m7j9njldwwzlg9v7v53unlr4jkmx6ey65nvtks5",
-      test: "kaspatest:pr6m7j9njldwwzlg9v7v53unlr4jkmx6eyvwc0uz5t",
-      dev: "kaspadev:qr6m7j9njldwwzlg9v7v53unlr4jkmx6eylep8ekg2",
-    },
-  },
-];
-
-// let myKey = bitcore.PrivateKey.fromString(kaspaKeyPairs[0].privKey); // error: not base58
-
-let myKey = bitcore.Address(kaspaKeyPairs[0].privKey);
-
-// test("prototype: generate random HDPrivateKey, derive child, encode/decode to cashaddr format", () => {
-//   let parent = new bitcore.HDPrivateKey(networkAlias);
-//   // TODO: verify that deriveChild() should be used over derive()
-//   let child_0_1_2h = parent.deriveChild("m/0'");
-//   let address = child_0_1_2h.privateKey.toAddress();
-//   let cashaddr = address.toCashAddress();
-//   expect(cashaddr).toContain("kaspatest:");
-//   expect(bitcore.Address(cashaddr).toString()).toEqual(address.toString());
-// });
+test("HD wallet should derive child addresses and encode to cashaddr with correct network prefix", () => {
+  let parent = new bitcore.HDPrivateKey();
+  for(let i = 0; i < 10; i++) {
+    let child = parent.deriveChild(`m/44'/972/0'/0'/${i}`);
+    networkPrefixes.forEach(prefix => {
+      expect(bitcore.Address.isValid(child.privateKey.toAddress(prefix))).toBe(true)
+      expect(child.privateKey.toAddress(prefix).toString()).toContain(prefix);
+      expect(child.depth).toEqual(5);
+    });
+  }
+})
