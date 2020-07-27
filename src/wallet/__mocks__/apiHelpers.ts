353b8b0340ffde6d0bc7dbe37046358661bbb204
wildmolasses <changes@gmail.com>
2020-07-27 15:07:54 -0500

    improving mocks

---
+++ src/wallet/__mocks__/apiHelpers.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+module.exports = {
+  getBlock: jest.fn((blockHash) =>
+    Promise.resolve({ utxos: require('../../../test/jest/data/blocks')[blockHash] || [] })
+  ),
+  getTransactions: jest.fn((address) =>
+    Promise.resolve({
+      transactions: require('../../../test/jest/data/transactions')[address] || [],
+    })
+  ),
+  getUtxos: jest.fn((address) => {
+    console.log(address);
+    return Promise.resolve({ utxos: require('../../../test/jest/data/utxos')[address] || [] });
+  }),
+  postTx: jest.fn((rawTx) => Promise.resolve(true)),
+};
